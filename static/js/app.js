55b6255319e10c998f8d122ac46df1fdec8950ed
DanielDesmondUCD <63327869+DanielDesmondPython@users.noreply.github.com>
2020-10-14 00:17:38 -0700

    select input and button

--- static/js/app.js
+++ static/js/app.js
@@ -9,12 +9,16 @@ console.log('what is up');
 //loop through table
 function displayData(data){
     tbody.text('')
-    data.forEach(function(et_sighting){
+    data.forEach(function(sighting){
     new_tablerow=tbody.append('tr')
-    Object.entries(et_sighting).forEach(function([key,value]){
+    Object.entries(sighting).forEach(function([key,value]){
         new_tabledata = new_tablerow.append('td').text(value)
 
     })
 })}
 
 displayData(tableData)
+
+//select input and button
+var input = d3.select('#datetime')
+var button = d3.select("filter-btn")
